036c931ba3f9aa5a5c0ccbc70f6d670408e35f93
mariano schiappacasse <mariano.schiappacasse@teco.com.ar>
2021-02-03 23:04:00 -0300

    1

---
+++ mariano/src/main/java/com/Conexion.java
@@ -0,0 +1,26 @@
+package com;
+
+//package galaxia;
+import java.sql.*;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class Conexion {
+   private static Connection cnx = null;
+   public static Connection obtener() throws SQLException, ClassNotFoundException {
+      if (cnx == null) {
+         try {
+        	 Class.forName("com.mysql.cj.jdbc.Driver");
+            cnx = DriverManager.getConnection("jdbc:mysql://34.72.218.208:3306/galaxia","galaxia", "Misofsan18");
+         } catch (SQLException ex) {
+            throw new SQLException(ex);
+         }
+      }
+      return cnx;
+   }
+   public static void cerrar() throws SQLException {
+      if (cnx != null) {
+         cnx.close();
+      }
+   }
+}
